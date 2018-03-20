295f6ff9bb6e567ade9b1f398e607f1e1c3e1ba1
Veronica Bustabad <vbustabad@gmail.com>
2018-03-20 02:33:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,20 @@ class Song
         genre_count[genre] += 1
       else
          genre_count[genre] = 1
-      end 
+      end
     end
     genre_count
-  end  
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
