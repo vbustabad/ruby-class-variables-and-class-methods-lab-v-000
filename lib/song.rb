0eacb7b11562ffe54cf1b81c08ca57f6d0c4687f
Veronica Bustabad <vbustabad@gmail.com>
2018-03-20 01:43:45 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genre_count.each do |genre, number_of_songs|
+      genre_count << {genre, number_of_songs}
+    end 
+  end
+
+end
