9bde10e86c3e113b35a85c92a210233b891821e3
Veronica Bustabad <vbustabad@gmail.com>
2018-03-20 02:31:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,11 +31,12 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-
-
-      genre_count << {genre => number_of_songs}
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+         genre_count[genre] = 1
+      end 
     end
     genre_count
-  end
-
+  end  
 end
